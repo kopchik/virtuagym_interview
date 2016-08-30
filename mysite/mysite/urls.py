6b7f4a59f9ca84547cdf7b4913e4e961e25c8c9a
Alexandre Kandalintsev <exe.sre@gmail.com>
2016-08-31 00:08:18 +0200

    support admin site and logout

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import logout
 
 
 urlpatterns = [
     url(r'^app/', include('app.urls')),
-    #url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^logout$', logout, {'next_page': '/'})
 ]
