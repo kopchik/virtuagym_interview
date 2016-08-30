3b86b6749b1425f95eae0e770b7ca842637d25f1
Alexandre Kandalintsev <exe.sre@gmail.com>
2016-08-31 00:07:41 +0200

    refactored JSX code

--- mysite/static/index.jsx
+++ mysite/static/index.jsx
@@ -1,53 +1,86 @@
-'use strict';
+'use strict'
 
-// PLAN (commentbox)
-// Excersizes
-// Days
-
-var $ = require("jquery")
+var $ = require('jquery')
 var React = require('react')
 var ReactDOM = require('react-dom')
-import Form from 'grommet/components/Form'
-import FormField from 'grommet/components/FormField'
-import Tiles from 'grommet/components/Tiles'
-import Tile from 'grommet/components/Tile'
+var Cookies = require('js-cookie')
+
+
 import App from 'grommet/components/App'
 import Header from 'grommet/components/App'
 import Title from 'grommet/components/Title'
+
+import Section from 'grommet/components/Layer'
+import Tiles from 'grommet/components/Tiles'
+import Tile from 'grommet/components/Tile'
+import Layer from 'grommet/components/Layer'
+import Table from 'grommet/components/Table'
+
+import Form from 'grommet/components/Form'
+import FormField from 'grommet/components/FormField'
+
+import DateTime from 'grommet/components/DateTime'
 import Button from 'grommet/components/Button'
 import Edit from 'grommet/components/icons/base/Edit'
-import DateTime from 'grommet/components/DateTime'
-import Table from 'grommet/components/Table'
-import Layer from 'grommet/components/Layer'
+import Spinning from 'grommet/components/icons/Spinning'
+
+
+// setup CSRF (from https://docs.djangoproject.com/en/1.10/ref/csrf/)
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
+        }
+    }
+});
 
 
 var MyApp = React.createClass({
   render: function() {
     return (
-      <App>
-        <Header pad="large">
+      <App inline={true}>
+        <Header direction="row" large={true} pad="large">
           <Title>Fitness Plans</Title>
         </Header>
-        <PlanList url="/app/plans/" />,
-        <Button label="add plan" />
-
+        <Section pad={{horizontal: 'medium'}}>
+        <Plans />
+        </Section>
       </App>
     )
   }
 })
 
+// TODO
+var LoginForm = React.createClass({
+  render: function() {
+    return(
+           <Layer onClose={false} closer={true} align="top">
+           <LoginForm onSubmit={false} />
+           </Layer>
+           )
+  }
+})
 
-var PlanList = React.createClass({
+
+var Plans = React.createClass({
   getInitialState: function() {
-    return {data: []};
+    return {data: [], edit: false, create: false};
   },
-  componentDidMount: function() {
+  getDefaultProps: function() {
+    return {url: "/app/plans"}
+  },
+  getData: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
       cache: false,
       success: function(data) {
-        console.log("PlanList AJAX:", data)
+        console.log("Plans AJAX:", data)
         this.setState({data: data.results});
       }.bind(this),
       error: function(xhr, status, err) {
@@ -55,80 +88,140 @@ var PlanList = React.createClass({
       }.bind(this)
     });
   },
+  componentDidMount: function() {
+    this.getData()
+  },
+  onPlanEdit: function(plan) {
+    console.log("onEdit", plan)
+    this.setState({edit: plan})
+  },
+  onPlanAdd: function() {
+    this.setState({create: true})
+  },
+  onEditorSave: function(plan) {
+    this.getData()
+    this.setState({edit: false, create: false})
+  },
+  onEditorCancel: function() {
+    this.setState({edit: false, create: false})
+  },
   render: function() {
-    var plans = this.state.data.map((e) => {
-      console.log("adding new plan, rawdata:", e)
-      return <Button label={e.title} icon={<Edit />} fill={true} key={e.url} />
-    })
+    var editor;
+    if (this.state.edit) {
+      editor = <Editor data={this.state.edit} onSave={this.onEditorSave} onCancel={this.onEditorCancel} />
+    } else if (this.state.create) {
+      editor = <Editor onSave={this.onEditorSave} onCancel={this.onEditorCancel} />
+    } else {
+      editor = null;
+    }
 
+    var plans = this.state.data.map((e) => {
+      var onClickHandler = () => this.onPlanEdit(e)
+      return <Button label={e.title} icon={<Edit />} fill={true} key={e.url} onClick={onClickHandler} />
+    })
     return (
-      <Tiles fill={true}>
-        <Tile align="start" pad="small">{plans}</Tile>
-        <Tile pad="small"><NewForm /></Tile>
-      </Tiles>
+      <Form>
+        <FormField label="Plans">
+          <Tiles fill={true}>
+            <Tile align="start" pad="small">
+              {plans}
+              {editor}
+            </Tile>
+          </Tiles>
+        </FormField>
+        <FormField>
+          <Button label="Add Plan" onClick={this.onPlanAdd} />
+        </FormField>
+      </Form>
     )
   }
 });
 
 
-var NewForm = React.createClass({
+var emptyPlan = {url: null, title: "", days: []}
+var Editor = React.createClass({
   getInitialState: function() {
-    return {data: [], date: "", show: true};
+    return {data: null, excersizes: [], date: ""};
   },
   getDefaultProps: function() {
-    return {
-      url: "/app/exercises/"
-    };
-  },
-  show: function() {
-    this.setState({show: true})
-  },
-  hide: function() {
-    this.setState({show: false})
+    return {data: null, excersizes_url: "/app/exercises/", plans_url: "/app/plans/"}
   },
   componentDidMount: function() {
-    $.ajax({
-      url: this.props.url,
+    this.getExcersizes()
+    if (this.props.data) {
+      this.setState({data: this.props.data})
+    } else {
+      this.createNewPlan()
+    }
+  },
+  createNewPlan: function() {
+    $.post({
+      url: this.props.plans_url,
       dataType: 'json',
-      cache: false,
+      data: {title: "new title"},
+      headers: {
+        "Authorization": "Basic " + window.btoa("user:password"),
+      },
       success: function(data) {
-        console.log("XXX LOADED", data);
         this.setState({data: data.results});
       }.bind(this),
       error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
+        console.error("cannot create new plan:", status, err.toString());
       }.bind(this)
     });
   },
-  onDateChange: function(date, e2, e3) {
-    console.log("ON DATE CHANGE:", date, e2, e3, this)
+  onDateChange: function(date, e2) {
+    console.log("ON DATE CHANGE:", date, e2, this)
     this.setState({date: date})
   },
-  onAddDay: function() {
-
+  onDayAdd: function() {
+    TODO
   },
-  onSave: function() {
-    this.hide()
-  },
-  onCancel: function() {
-    this.hide()
+  getExcersizes: function() {
+    $.ajax({
+      url: this.props.excersizes_url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({excersizes: data.results});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error("cannot get excersizes:", status, err.toString());
+      }.bind(this)
+    });
   },
   render: function() {
-    var excersizes = this.state.data.map(function(e) {
-      // console.log("EXCERSIZE:", e)
+    // data not loaded yet, show spinning wheel
+    if (!this.state.data) {
+      return (
+        <Layer align="left" onClose={this.hide} closer={true}>
+          <Spinning />
+        </Layer>
+      )
+    }
+
+    var excersizes = this.state.excersizes.map(function(e) {
       return <tr key={e.url}><td>{e.name}</td></tr>
     })
-    if (!this.state.show) {
-      return null
-    }
+    var days = this.state.data.days.map((day) => <tr key={day.url}><td>{day.date}</td></tr>)
+    var title = title = this.state.data.title
+
     return (
       <Layer align="left" onClose={this.hide} closer={true}>
         <Form compact={true} pad={{vertical: "large", between: "large"}}>
               <FormField label="Plan Name" htmlFor="item1">
                 <input id="item1" type="text" />
               </FormField>
-              <Button label="Add Day" onClick={this.onAddDay} />
-
+              <FormField>
+              <FormField label="days">
+                <Table selectable={true}>
+                  <tbody>
+                    {days}
+                  </tbody>
+                </Table>
+              </FormField>
+                <Button label="Add Day" onClick={this.onDayAdd} />
+              </FormField>
               <FormField label="Date">
                 <DateTime format="M/D/YYYY" value={this.state.date} onChange={this.onDateChange} id="item1" name="item-1" />
               </FormField>
@@ -140,27 +233,13 @@ var NewForm = React.createClass({
                     </tbody>
                   </Table>
               </FormField>
-              <Button label="Save" onClick={this.onSave} />
+              <FormField>
+                <Button label="Save" onClick={this.props.onSave} />
+                <Button label="Cancel" onClick={this.props.onCancel} />
+              </FormField>
         </Form>
       </Layer>
     )
-  }
-});
-
-
-var Plan = React.createClass({
-  getInitialState: function() {
-    return {data: {title: "", days: []}};
-  },
-  handleClick: function() {
-    alert("HABA")
-  },
-  render: function() {
-    return (
-      <div className="plan" onClick={this.handleClick}>
-        <h1>{this.props.title}</h1>
-      </div>
-    );
   }
 });
 
