ca6c80d914446a1986c33c5f5a2ef1fd6d9be994
Alexandre Kandalintsev <exe.sre@gmail.com>
2016-09-02 11:37:02 +0200

    make nested serializers working

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -4,17 +4,15 @@ from django.db import models
 class Plan(models.Model):
     title = models.CharField(max_length=255)
 
-    def get_days(self):
-        return Day.objects.filter(plan=self)
-
     def __str__(self):
         return self.title
 
 
 class Day(models.Model):
     date = models.DateField()
-    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
-    exercises = models.ManyToManyField('Exercise')
+    plan = models.ForeignKey(Plan, related_name='days',
+                             blank=True, on_delete=models.CASCADE)
+    exercises = models.ManyToManyField('Exercise', blank=True)
 
     def __str__(self):
         plan = self.plan.title if self.plan else "(no plan)"
--- mysite/app/serializers.py
+++ mysite/app/serializers.py
@@ -15,8 +15,42 @@ class ExerciseSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class PlanSerializer(serializers.HyperlinkedModelSerializer):
-    days = DaySerializer(source='get_days', many=True)
+    days = DaySerializer(many=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     class Meta:
         model = Plan
-        field = ('days',)
+        field = ['days']
+
+    def create(self, validated_data):
+        days_data = validated_data.pop('days')
+        plan = Plan.objects.create(**validated_data)
+        # nested models need to be created separately :(
+        for day_data in days_data:
+            exercises = day_data.pop('exercises')
+            day = Day.objects.create(plan=plan, **day_data)
+            for exercise in exercises:
+                day.exercises.add(exercise)
+        return plan
+
+    def update(self, instance, validated_data):
+        days_data = validated_data.pop('days')
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+
+        # we update days by removing them...
+        for old_day in instance.days.all():
+            old_day.delete()
+
+        # ... and creating them back again
+        for new_day in days_data:
+            new_day.pop('plan')  # we'll use 'instance' that's already saved
+            exercises = new_day.pop('exercises')
+            day = Day.objects.create(plan=instance, **new_day)
+            for exercise in exercises:
+                day.exercises.add(exercise)
+
+        return instance
