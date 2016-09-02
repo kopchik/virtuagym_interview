705cd5250e6a458eb25ec8af95b1294b44ca60b6
Alexandre Kandalintsev <exe.sre@gmail.com>
2016-09-02 11:36:13 +0200

    added support for some logging stuff

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from app.views import PlanViewSet, DayViewSet, ExerciseViewSet
+from app.views import PlanViewSet, DayViewSet, ExerciseViewSet, CurrentUserView
 
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import response, schemas
@@ -12,6 +12,7 @@ router = routers.DefaultRouter()
 router.register(r'plans', PlanViewSet)
 router.register(r'Days', DayViewSet)
 router.register(r'exercises', ExerciseViewSet)
+#router.register(r'me', CurrentUserView, base_name='app')
 urlpatterns = router.urls
 
 
@@ -24,7 +25,6 @@ def schema_view(request):
     return response.Response(schema)
 
 urlpatterns += [
-    url(r'api/', schema_view, name='the-api'),
+    url(r'api/', schema_view, name='another-api-view'),
+    url(r'me/', CurrentUserView.as_view(), name='current-user'),
 ]
-
-
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.contrib.auth import authenticate
 
 from app.models import Plan, Day, Exercise
 from app.serializers import PlanSerializer, DaySerializer, ExerciseSerializer
@@ -19,3 +24,19 @@ class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercise.objects.all().order_by('-id')
     serializer_class = ExerciseSerializer
 
+
+class CurrentUserView(APIView):
+    """ Quick-n-dirty view to detect if user is already logged in. """
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return JsonResponse({'username': user.username})
+        return HttpResponse('Unauthorized', status=403)
+
+    def get(self, request):
+        user = request.user
+        return JsonResponse({'username': user.username})
